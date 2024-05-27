abf245c7e2ad04a53895a2179ea950bbb3458ecf
ifm-sg <167881590+ifm-sg@users.noreply.github.com>
2024-05-27 15:01:53 +0200

    Update pinecone.js

--- pinecone.js
+++ pinecone.js
@@ -1,4 +1,5 @@
-var ajaxCall = (key, values) => {
+// Function to make an AJAX call to the Pinecone API
+var pineconeAjaxCall = (key, values) => {
   return new Promise((resolve, reject) => {
     $.ajax({
       url: 'https://sample-movies-9lkj2gj.svc.aped-4627-b74a.pinecone.io/query',
@@ -29,22 +30,34 @@ var ajaxCall = (key, values) => {
 };
 
 (function () {
-  const template = document.createElement("template");
-  template.innerHTML = `
+  const pineconeTemplate = document.createElement("template");
+  pineConeTemplate.innerHTML = `
       <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;">
       </div>
     `;
-  class MainWebComponent extends HTMLElement {
+  
+  class PineconeWebComponent extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.appendChild(pineconeTemplate.content.cloneNode(true));
+    }
     async post(apiKey, values) {
-      const { response } = await ajaxCall(
-        apiKey,
-        values
-      );
-      console.log(response);
-      return response.matches.metadata.text;
+      // Validate inputs
+      if (!apiKey || !Array.isArray(values) || values.length === 0) {
+        throw new Error('API key and non-empty values array are required.');
+      }
+      try {
+        const { response } = await pineconeAjaxCall(apiKey, values);
+        console.log(response);
+        return response.matches.metadata.text;
+      } catch (error) {
+        console.error('Error in post method:', error);
+        throw error;
+      }
     }
   }
-  customElements.define("custom-widget-pinecone", MainWebComponent);
+  customElements.define("custom-widget-pinecone", PineconeWebComponent);
 })();
