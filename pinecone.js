ebaa65beb55685ab49e5938d71e7463fb1492efc
ifm-sg <167881590+ifm-sg@users.noreply.github.com>
2024-06-03 16:35:44 +0200

    Update pinecone.js

--- pinecone.js
+++ pinecone.js
@@ -1,13 +1,14 @@
 // Function to make an AJAX call to the Pinecone API
-var pineconeAjaxCall = (key, indexHost, vector) => {
+var pineconeAjaxCall = (key, indexHost, vector, countryFilter, yearFilter) => {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: `https://${indexHost}/query`,
+      url: `${indexHost}/query`,
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
         vector: vector,
-        //filter: {"genre": {"$eq": "documentary"}},
+        filter: {"country": {"$in": countryFilter},
+                "year": {"$in": yearFilter},
         topK: 5,
         includeValues: false,
         includeMetadata: true
@@ -44,19 +45,26 @@ var pineconeAjaxCall = (key, indexHost, vector) => {
       this.attachShadow({ mode: 'open' });
       this.shadowRoot.appendChild(pineconeTemplate.content.cloneNode(true));
     }
-    async post(apiKey, indexHost, vector) {
+    async post(apiKey, indexHost, vector, countryFilter, yearFilter) {
       // Validate inputs
-      if (!apiKey || !indexHost || !Array.isArray(vector) || vector.length === 0) {
-        throw new Error('API key, index host and non-empty vector array are required.');
+      if (!apiKey || !indexHost || !Array.isArray(vector) || vector.length === 0 || !Array.isArray(countryFilter) || countryFilter.length === 0 || !Array.isArray(yearFilter) || yearFilter.length === 0) {
+        throw new Error('API key, index host, non-empty vector / country filter / year filter array are required.');
       }
       try {
-        const { response } = await pineconeAjaxCall(apiKey, indexHost, vector);
+        const { response } = await pineconeAjaxCall(apiKey, indexHost, vector, countryFilter, yearFilter);
         console.log(response);
         let content = "";
         for (let i = 0; i < response.matches.length; i++) {
           content += ` Option ${i}: ${response.matches[i].metadata.text};`;
         }
-        return content;
+        let ids = "";
+        for (let i = 0; i < response.matches.length; i++) {
+          ids += ` ${response.matches[i].id},`;
+        }
+        let responseArray = [];
+        responseArray.push(content);
+        responseArray.push(ids);
+        return responseArray;
       } catch (error) {
         console.error('Error in post method:', error);
         throw error;
